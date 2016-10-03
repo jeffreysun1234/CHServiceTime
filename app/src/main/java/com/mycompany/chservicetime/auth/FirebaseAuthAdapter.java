9ac8fd9c56649b1d5d190c93592e6eea74c3239c
szhx <szhx@mycomputer>
2016-10-03 11:08:35 -0400

    Squashed commit of the following:
    commit be4000ec12834debed540257c69631add40bb710
    Author: szhx <szhx@mycomputer>
    Date:   Tue Sep 27 13:32:56 2016 -0400
    
        1. Code static review
        2. Permision ( Android 6.0 )
    
    commit fb08b370033d4849e6bae2e749ed6a7e8c861eb8
    Author: szhx <szhx@mycomputer>
    Date:   Tue Sep 20 22:48:56 2016 -0400
    
        Integrate Spoon test tool
    
    commit 8eb23a1994668aa1b5e8d038af745f14619c689e
    Author: szhx <szhx@mycomputer>
    Date:   Mon Sep 19 19:41:28 2016 -0400
    
        1. Integrate UiAutomator lib
        2. Test RingMode
    
    commit 6c22909ef535aac2a50532437a7dfc52581f7ca9
    Author: szhx <szhx@mycomputer>
    Date:   Sat Sep 10 21:57:29 2016 -0400
    
        Add Backup and Restore the TimeSlot list to Firebase using Rest service
    
    commit f9e2d4885252b99e808f6175af2545cae9b49b60
    Author: szhx <szhx@mycomputer>
    Date:   Fri Sep 9 10:06:54 2016 -0400
    
        Firebase REST test
    
    commit 6fce3760feb3650331691f433f95eb042f68c467
    Author: szhx <szhx@mycomputer>
    Date:   Thu Sep 8 23:13:18 2016 -0400
    
        Integrate Firebase REST, using retrofit 2 and gson
    
    commit 29d76cdb1e91470e4057209dda5e1ada9bf61f99
    Author: szhx <szhx@mycomputer>
    Date:   Mon Sep 5 19:25:33 2016 -0400
    
        Integrate Firebase autherization ( FirebaseUI lib)
    
    commit 24d35597d4681fa7207da7d1a4c04b7f014a0cff
    Author: szhx <szhx@mycomputer>
    Date:   Fri Sep 2 23:35:27 2016 -0400
    
        add Markdown(.md) files

---
+++ app/src/main/java/com/mycompany/chservicetime/auth/FirebaseAuthAdapter.java
@@ -0,0 +1,81 @@
+package com.mycompany.chservicetime.auth;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.mycompany.chservicetime.CHApplication;
+import com.mycompany.chservicetime.R;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by szhx on 9/7/2016.
+ */
+public class FirebaseAuthAdapter {
+
+    public static FirebaseAuth FIREBASE_AUTH;
+    public static FirebaseUser FIREBASE_USER;
+
+    public static boolean isSignIn() {
+        return FIREBASE_USER != null;
+    }
+
+    public static String getEmail() {
+        return FIREBASE_USER.getEmail();
+    }
+
+    public static void showLoginHintInSnackbar(View view) {
+        if (!isSignIn())
+            Snackbar.make(view, CHApplication.getContext().getString(R.string.sign_in_hint), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+    }
+
+    /**
+     * This is a asynchronous operation.
+     *
+     * @return
+     */
+    public static String getAuthToken() {
+        final String[] authToken = new String[1];
+
+        if (isSignIn()) {
+            final CountDownLatch latch = new CountDownLatch(1);
+
+            Task<GetTokenResult> getTokenResultTask = FIREBASE_USER.getToken(true)
+                    .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<GetTokenResult> task) {
+                            if (task.isSuccessful()) {
+                                authToken[0] = task.getResult().getToken();
+                            }
+
+                            latch.countDown();
+                        }
+                    });
+
+            try {
+                latch.await(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            authToken[0] = getTokenResultTask.getResult().getToken();
+        } else {
+            return authToken[0] = null;
+        }
+
+        return authToken[0];
+    }
+
+    public static String getUserId() {
+        return FIREBASE_USER.getUid();
+    }
+}
